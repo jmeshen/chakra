e5e7e599e17ccf1f648ceb3ba03bcae63be94058
jmeshen <jmeshen@gmail.com>
2014-12-26 22:50:25 -0500

    modified css, js, html

--- chak.js
+++ chak.js
@@ -1,4 +1,17 @@
-$(document).ready(function(){
-  alert("Herro");
+/*$(document).ready(function(){
+  $(".chakdiv").on("mouseenter", function(){
+    $(this).animate({right:"200px"});
+  });
+	$(".chakdiv").on("mouseleave", function(){
+		$(this).animate({right:"0px"});
+	});
+}); 2014-12-26 JXS | cool bouncing mechanism */
 
+$(document).ready(function(){
+  $(".chakdiv").click(function(){
+    var curId = $(this).attr("id");
+    //alert(curId);
+    $(this).animate({right:"200px"});
+    $(this).parent().find("#" + curId + "Desc").show().animate({right:"-125px", top:"-60px"});
+  });
 });
